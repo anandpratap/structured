9adc7ec860860bead9c3d21b858acaf45b43c65c
Anand Pratap Singh <anand.deshwali@gmail.com>
2016-09-02 12:17:53 -0400

    added transport support

--- src/solver/solution.h
+++ src/solver/solution.h
@@ -12,7 +12,7 @@ class Mesh;
 template<class T>
 class Solution{
  public:
-	uint nic, njc, nq, naux;
+	uint nic, njc, nq, naux, ntrans;
 	Array3D<T> q;
 	Array3D<T> q_aux;
 public:
@@ -24,11 +24,12 @@ public:
 template<class T>
 Solution<T>::Solution(auto mesh){
 	nq = 4;
+	ntrans = 0;
 	naux = 10;
 	nic = mesh->nic;
 	njc = mesh->njc;
 
-	q = Array3D<T>(nic, njc, nq);
+	q = Array3D<T>(nic, njc, nq+ntrans);
 	q_aux = Array3D<T>(nic, njc, naux);
 
 	for(uint i=0; i<nic; i++){
@@ -37,6 +38,9 @@ Solution<T>::Solution(auto mesh){
 			q[i][j][1] = 0.8;
 			q[i][j][2] = 0.0;
 			q[i][j][3] = 2.1057142857142863;
+			for(uint tn=0; tn<ntrans; tn++){
+				q[i][j][tn+4] = 1.0;
+			}
 		}
 	}
 }
