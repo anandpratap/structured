77ac18acb338f61ee89e86df03a6a90c237259ad
Anand Pratap Singh <anand.deshwali@gmail.com>
2016-09-19 14:55:04 -0400

    Added calcualtions of cl cd

--- src/utils/io.h
+++ src/utils/io.h
@@ -177,11 +177,14 @@ public:
 		auto q = mesh->solution->q;
 		auto xc = mesh->xc;
 		auto yc = mesh->yc;
+		auto x = mesh->xv;
+		auto y = mesh->yv;
+
 		Tx p_inf = 1/1.4;
 		Tx rho_inf = 1.0;
 		Tx u_inf = 0.5;
 		int j1 = mesh->j1-1;
-		Tx x, cp;
+		Tx xw, cp;
 		std::ofstream outfile;
 		outfile.open(filename);
 		auto rho = mesh->solution->rho;
@@ -189,11 +192,50 @@ public:
 		auto v = mesh->solution->v;
 		auto p = mesh->solution->p;
 		auto T = mesh->solution->T;
-		for(uint i=j1; i<j1+mesh->nb; i++){
-			x = xc[i][0];
-			cp = (p[i][0] - p_inf)/(0.5*rho_inf*u_inf*u_inf);
-			outfile<<x<<" "<<cp<<std::endl;
+
+
+		auto Fn_pressure = 0.0;
+		auto Fc_pressure = 0.0;
+
+		auto Fn_viscous = 0.0;
+		auto Fc_viscous = 0.0;
+
+		auto grad_u = mesh->equation->grad_u_eta;
+		auto grad_v = mesh->equation->grad_v_eta;
+			
+		for(int i=j1; i<j1+mesh->nb; i++){
+			xw = xc[i][0];
+			auto qinf = (0.5*rho_inf*u_inf*u_inf);
+			cp = (0.5*(p[i][0] + p[i][1]) - p_inf)/qinf;
+			auto mu = 1e-4;
+			auto tau = mu*(grad_u[i][0][1].value() - grad_v[i][0][0].value())/qinf;
+			auto cf = tau;
+			outfile<<xw<<" "<<cp<<" "<<cf<<std::endl;
+			auto dx = (x[i+1][0] - x[i][0]);
+			auto dy = (y[i+1][0] - y[i][0]);
+			Fn_pressure = Fn_pressure - cp*dx;
+			Fc_pressure = Fc_pressure + cp*dy;
+
+			auto sfdiv = 2.0/3.0*(grad_u[i][0][0].value() + grad_v[i][0][1].value());
+			auto sxx = mu*(2.0*grad_u[i][0][0].value() - sfdiv)/qinf;
+			auto syy = mu*(2.0*grad_v[i][0][1].value() - sfdiv)/qinf;
+			Fn_viscous = Fn_viscous - tau*dy + syy*dx;
+			Fc_viscous = Fc_viscous + tau*dx - sxx*dy;
 		}
+		auto ca = 1.0;
+		auto sa = 0.0;
+		auto cl_pressure = -Fc_pressure*sa + Fn_pressure*ca;
+		auto cd_pressure = Fc_pressure*ca + Fn_pressure*sa;
+
+		auto cl_viscous = -Fc_viscous*sa + Fn_viscous*ca;
+		auto cd_viscous = Fc_viscous*ca + Fn_viscous*sa;
+
+		auto cl = cl_viscous + cl_pressure;
+		auto cd = cd_viscous + cd_pressure;
+		spdlog::get("console")->info("Pressure Cl: {} Cd: {}", cl_pressure, cd_pressure);
+		spdlog::get("console")->info("Viscous Cl: {} Cd: {}", cl_viscous, cd_viscous);
+		spdlog::get("console")->info("Total Cl: {} Cd: {}", cl, cd);
+
 		outfile.close();
 		spdlog::get("console")->info("Wrote surface file {}.", filename);
 	}
